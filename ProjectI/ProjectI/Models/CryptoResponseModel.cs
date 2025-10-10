611bc883a95021f9d1b2aa179a1033cac762916e
Erand Kurtaliqi <Erand Kurtaliqi@DESKTOP-2NPHQQH>
2025-10-10 22:26:36 +0200

    Creating a response model for the blowfish algorithm

---
+++ ProjectI/ProjectI/Models/CryptoResponseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ProjectI.Models
+{
+    public class CryptoResponseModel
+    {
+    }
+}
--- ProjectI/ProjectI/obj/Debug/net8.0/ProjectI.AssemblyInfo.cs
+++ ProjectI/ProjectI/obj/Debug/net8.0/ProjectI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a847ae5237051500f3bf0b92dad7f2ee900c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb64d8b2c7daabb088e9ce769da6b0b867d55416")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
