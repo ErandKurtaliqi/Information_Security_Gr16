10c0331ede27fcfec9581a11d5564228deb24c07
Besarta Berisha <besarta.berisha1@student.uni-pr.edu>
2025-10-15 15:06:32 +0200

    Encryption key generation function

--- ProjectI/ProjectI/Structure_Class/Encrypt_Class.cs
+++ ProjectI/ProjectI/Structure_Class/Encrypt_Class.cs
@@ -1,6 +1,20 @@
-﻿namespace ProjectI.Structure_Class
+﻿using System.Text;
+
+namespace ProjectI.Structure_Class
 {
     public class Encrypt_Class
     {
+
+        private byte[] DeriveKey(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                // Mund ta shkurtosh ose zgjatësh sipas nevojës (maks 56 byte për Blowfish)
+                byte[] key = new byte[32]; // 32 byte = 256 bit
+                Buffer.BlockCopy(hash, 0, key, 0, key.Length);
+                return key;
+            }
+        }
     }
 }
