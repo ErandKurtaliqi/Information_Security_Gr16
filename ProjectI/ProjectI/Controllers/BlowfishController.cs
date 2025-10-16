c764abceb4695e8a135537592cd93b90b599a7e0
Erand Kurtaliqi <Erand Kurtaliqi@DESKTOP-2NPHQQH>
2025-10-16 14:33:47 +0200

    Creating BlowfishController, as the main controller which is connected to ControllerBase, to implement the algorithm

---
+++ ProjectI/ProjectI/Controllers/BlowfishController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlowfishController : ControllerBase
+    {
+    }
+}
