2ece069d57513c631a4ba7f727f57679d9686ba0
BrahimSylejmani <bimisylejmani56@gmail.com>
2025-11-02 14:45:00 +0100

    Implemented controller for BlowFish service

--- ProjectI/ProjectI/Controllers/BlowfishController.cs
+++ ProjectI/ProjectI/Controllers/BlowfishController.cs
@@ -8,7 +8,35 @@ namespace ProjectI.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class BlowfishController : ControllerBase
-    {
-       
+    {  
+       [HttpPost("encrypt")]
+        public async Task<ActionResult<CryptoResponseModel>> Encrypt([FromBody] EncryptRequestModel req)
+        {
+            try
+            {
+                string result;
+
+                var keyBase64 = string.IsNullOrWhiteSpace(req.KeyBase64)
+                    ? _config["Blowfish:KeyBase64"]
+                    : req.KeyBase64;
+
+                var ivHex = string.IsNullOrWhiteSpace(req.IVHex)
+                    ? _config["Blowfish:IVHex"]
+                    : req.IVHex;
+
+                var key = Convert.FromBase64String(keyBase64!);
+                var iv = HexToBytes(ivHex!);
+
+                if (iv.Length != 8)
+                    return BadRequest("IV duhet 8 bajte (16 hexdigjit).");
+
+                result = await _svc.EncryptToBase64Async(req.Plaintext, key, iv);
+                return Ok(new CryptoResponseModel { Result = result });
+            }
+            catch (Exception ex)
+            {
+                return Problem($"Encrypt error: {ex.Message}");
+            }
+        }
     }
 }
