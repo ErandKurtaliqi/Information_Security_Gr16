aa1e882f2a7ca6d550132e6b444acbc2594bba92
Erand Kurtaliqi <Erand Kurtaliqi@DESKTOP-2NPHQQH>
2025-11-05 15:26:52 +0100

    Creating columns for the 'EncryptRequestModel' input model

--- ProjectI/ProjectI/Models/EncryptRequestModel.cs
+++ ProjectI/ProjectI/Models/EncryptRequestModel.cs
@@ -1,6 +1,11 @@
-﻿namespace ProjectI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectI.Models
 {
-    public class EncryptRequestModel
+    public sealed class EncryptRequestModel
     {
+        [Required] public string Plaintext { get; set; } = "";
+        public string? KeyBase64 { get; set; }
+        public string? IVHex { get; set; }
     }
 }
